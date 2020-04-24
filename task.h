05f5c75a3dbd465eb3c8e2bcd10ae7a19d09a4ed
simonojok19 <simonojok19@gmail.com>
2020-04-24 23:23:29 +0300

    started coding

---
+++ task.h
@@ -0,0 +1,25 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <QWidget>
+
+namespace Ui {
+    class Task;
+}
+
+class Task : public QWidget {
+    Q_OBJECT
+
+    public:
+        explicit Task(QWidget *parent = nullptr);
+        ~Task();
+
+        void setName(const QString& name);
+        QString name() const;
+        bool isCompleted() const;
+
+    private:
+        Ui::Task *ui;
+};
+
+#endif // TASK_H
